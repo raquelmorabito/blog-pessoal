286cc5b02c546ec20b74ba85b7ab4d4d461c2f1e
User <raquelmorabito@hotmail.com>
2024-12-16 00:09:50 -0300

    Implementação de Métodos CRUD e Busca Específica no Blog Pessoal

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -4,15 +4,21 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
@@ -33,6 +39,22 @@ public class PostagemController {
     	return ResponseEntity.ok(postagem.get()); 
     	} else {
     		return ResponseEntity.notFound().build(); 
-    		} 
-    } 
+    	}
+    }
+    		@GetMapping("/titulo/{titulo}")
+    		public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+    			return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCare(titulo));
+    		}
+    		@PostMapping
+    		public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+    				return ResponseEntity.status(HttpStatus.CREATED)
+    						.body(postagemRepository.save(postagem));
+    		}
+    		@PutMapping
+    		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+    				return postagemRepository.findById(postagem.getId())
+    						.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+    								.body(postagemRepository.save(postagem)))
+    						.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    		}
 }
